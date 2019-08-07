983c1afcfeaf1556b521eeb6aafa4e5f66398aeb
Gerson Navarro <soportedesarrollo1@losportales.com.pe>
2019-08-07 10:10:26 -0500

    refactor

--- ConnectSAPCore.Service/Controllers/ValuesController.cs
+++ ConnectSAPCore.Service/Controllers/ValuesController.cs
@@ -24,22 +24,5 @@ namespace ConnectSAPCore.Service.Controllers
             return Ok(result);
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
